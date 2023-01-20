8ba815da4ff12fbe0ae3afc3487ad74ebc2ebe89
kroilov <popsakiller@gmail.com>
2023-01-20 16:03:21 +0300

    practice 03

---
+++ practice_03/1.js
@@ -0,0 +1,3 @@
+let tCels = prompt("Enter temperature:");
+let tFahr = (9 / 5) * tCels + 32;
+alert(`${tCels}C = ${tFahr.toFixed(1)}F`);
---
+++ practice_03/2.js
@@ -0,0 +1,7 @@
+function greeting(name) {
+    console.log(`Hello, ${name}!`)
+}
+
+
+let uName = prompt("Tell me, what's your name?");
+greeting(uName);
